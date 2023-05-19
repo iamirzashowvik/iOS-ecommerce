001e4581f5e18d8eefe5856a1afc8f25885619b6
Mirza Showvik <showvikmirza@gmail.com>
2023-05-20 02:32:33 +0600

    fix all issues

---
+++ ecommerce/Views/CheckOut.swift
@@ -0,0 +1,20 @@
+//
+//  CheckOut.swift
+//  ecommerce
+//
+//  Created by Mirza Showvik on 19/5/23.
+//
+
+import SwiftUI
+
+struct CheckOut: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckOut_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckOut()
+    }
+}
