b37188bfe67345f7ace745abbdd3185f447f8056
Mirza Showvik <showvikmirza@gmail.com>
2023-05-19 18:55:28 +0600

    Initial Commit

---
+++ ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Mirza Showvik on 19/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
